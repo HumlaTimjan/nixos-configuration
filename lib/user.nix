9cd71e091431280a43a24d2b03cc9af136e7f0ae
Birger Rydback <birger@humla.io>
2022-12-07 13:25:42 +0100

    EN-632: Fix home manager lib

--- lib/user.nix
+++ lib/user.nix
@@ -2,38 +2,38 @@
 with builtins;
 {
   mkHMUser = {userConfig, username}:
+    let
+      trySettings = tryEval (fromJSON (readFile /etc/hmsystemdata.json));
+      machineData = if trySettings.success then trySettings.value else {};
+
+      machineModule = { pkgs, config, lib, ... }: {
+        options.machineData = lib.mkOption {
+          default = {};
+          description = "Settings passed from nixos system configuration. If not present will be empty";
+        };
+
+        config.machineData = machineData;
+      };
+    in
     home-manager.lib.homeManagerConfiguration {
-      inherit system username pkgs;
-      stateVersion = "21.11";
-      configuration =
-        let
-          trySettings = tryEval (fromJSON (readFile /etc/hmsystemdata.json));
-          machineData = if trySettings.success then trySettings.value else {};
+      inherit pkgs;
+      modules = [
+        {
+          imports = [ ../modules/users machineModule ];
 
-          machineModule = { pkgs, config, lib, ... }: {
-            options.machineData = lib.mkOption {
-              default = {};
-              description = "Settings passed from nixos system configuration. If not present will be empty";
-            };
-
-            config.machineData = machineData;
-          };
-        in {
           br = userConfig;
 
           nixpkgs.overlays = overlays;
           nixpkgs.config.allowUnfree = true;
 
           systemd.user.startServices = true;
-          home.stateVersion = "21.11";
+          home.stateVersion = "22.11";
           home.username = username;
           home.homeDirectory = "/home/${username}";
-
-          imports = [ ../modules/users machineModule ];
-        };
-      homeDirectory = "/home/${username}";
+        }
+      ];
     };
-
+      
   mkSystemUser = { name, groups, uid, shell, ... }:
   {
     users.users."${name}" = {
